b55197240d12159e20055b7bd65f8900883afd4b
Mateo Urquijo <58572533+Mattewska@users.noreply.github.com>
2023-10-10 07:57:46 -0500

    calculator practica

---
+++ src/app/Calculadora/calculadora.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-calculator",
+    template: "./calculadora.component.html",
+})
+
+export class Calculator {
+    item: number = 0;
+}
